2b19387436092691caa9a0297ec0ea68b3a9f9ca
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-07-09 00:07:09 +0100

    Implemented best_ratio from old code, added unit tests

--- basest/best_ratio.py
+++ basest/best_ratio.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from math import ceil, log
 
 
-def best_ratio(*args, **kwargs):
+INF = float('infinity')
+
+
+def _encoding_ratio(base_from, base_to, chunk_sizes):
     """
-    TODO: Better docstring!
+    An algorithm for finding the most efficient encoding ratio
+    from one base to another within a range limit.
     """
-    pass
+    best_ratio = (1.0, INF)
+    for s in chunk_sizes:
+        match = ceil(log(base_from ** s, base_to))
+        ratio = (float(s), match)
+        if (ratio[0] / ratio[1]) > (best_ratio[0] / best_ratio[1]):
+            best_ratio = ratio
+    return (int(best_ratio[0]), int(best_ratio[1]))
+
+
+def best_ratio(input_base, output_bases, chunk_sizes):
+    """
+    For a given input base and a range of acceptable output bases and chunk
+    sizes, find the most efficient encoding ratio.
+    Returns the chosen output base, and the chosen encoding ratio.
+    """
+    encoder = 0
+    best_ratio = (1.0, INF)
+    for base_to in output_bases:
+        ratio = _encoding_ratio(input_base, base_to, chunk_sizes)
+        if (
+            (float(ratio[0]) / float(ratio[1])) >
+            (float(best_ratio[0]) / float(best_ratio[1]))
+        ):
+            best_ratio = ratio
+            encoder = base_to
+    return encoder, (int(best_ratio[0]), int(best_ratio[1]))
--- tests/test_best_ratio.py
+++ tests/test_best_ratio.py
@@ -10,10 +10,14 @@ from basest import best_ratio
 @ddt
 class TestBestRatio(unittest.TestCase):
     @data(
-        (256, [94], range(256), (94, (92, 99))),
-        (256, [94], range(512), (94, (355, 382))),
-        (256, range(2, 95), range(256), (94, (92, 99))),
-        (256, range(2, 333), range(256), (333, (243, 232)))
+        (256, [94], range(1, 256), (94, (68, 83))),
+        (256, [94], range(1, 512), (94, (458, 559))),
+        (256, range(2, 95), range(1, 256), (94, (68, 83))),
+        (256, range(2, 334), range(1, 256), (333, (243, 232))),
+        # This is base-64's ratio, which should be 3:4
+        (256, [64], range(1, 10), (64, (3, 4))),
+        # This is base-85-s ratio, which should be 4:5
+        (256, [85], range(1, 10), (85, (4, 5)))
     )
     @unpack
     def test_best_ratio(self, input_base, output_bases, chunk_sizes, expected):
