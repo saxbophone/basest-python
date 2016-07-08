c79ae67ceef1e16cc045abe5a7d752b1e5fd88d7
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-07-08 23:46:38 +0100

    Currently failing test cases for the as-yet unimplemented best_ratio

--- tests/test_best_ratio.py
+++ tests/test_best_ratio.py
@@ -9,7 +9,19 @@ from basest import best_ratio
 
 @ddt
 class TestBestRatio(unittest.TestCase):
-    @data()
+    @data(
+        (256, [94], range(256), (94, (92, 99))),
+        (256, [94], range(512), (94, (355, 382))),
+        (256, range(2, 95), range(256), (94, (92, 99))),
+        (256, range(2, 333), range(256), (333, (243, 232)))
+    )
     @unpack
-    def test_best_ratio(self):
-        best_ratio()
+    def test_best_ratio(self, input_base, output_bases, chunk_sizes, expected):
+        """
+        Test that basest.best_ratio returns the correct expected outputs when
+        requested to find the best compression ratios and/or base-to-base
+        ratios.
+        """
+        self.assertEqual(
+            best_ratio(input_base, output_bases, chunk_sizes), expected
+        )
