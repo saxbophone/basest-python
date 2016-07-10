8be50daf12bb9f55d57c2086e5efd7527d14a685
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-07-10 01:17:48 +0100

    Completed decode test cases (Some test cases passing, others failing)

--- basest/decode.py
+++ basest/decode.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from .encode import encode
 
 
 def decode(
@@ -14,4 +15,26 @@ def decode(
     Assumes standard base64-style padding using the given input padding symbol,
     but can handle unpadded input just fine.
     """
-    raise NotImplementedError('This function not implemented yet')
+    input_length = len(input_data)
+    overlap = input_length % input_ratio
+    # first, check if the input data is not long enough and pad it if it isn't
+    if overlap > 0:
+        ...
+    # count number of padding characters
+    padding_length = input_data.count(input_padding)
+    # now, replace all padding characters with the symbol of index 0
+    input_data = [
+        (s if s != input_padding else input_symbol_table[0])
+        for s in input_data
+    ]
+    # use the encode function to convert the data
+    output_data = encode(
+        input_base=input_base, input_symbol_table=input_symbol_table,
+        output_base=output_base, output_symbol_table=output_symbol_table,
+        output_padding=None,
+        input_ratio=input_ratio, output_ratio=output_ratio,
+        input_data=input_data
+    )
+    # strip off the unnecessary trailing zeros if there was padding
+    [output_data.pop() for _ in range(padding_length)]
+    return output_data
