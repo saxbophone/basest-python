a89f5f0ec48f4c78b05717e7fdfd7baabee0550f
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-07-24 00:43:35 +0100

    Add a new raw_decode() function, with decode() now using it.

--- basest/core/decode.py
+++ basest/core/decode.py
@@ -4,7 +4,38 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-from .encode import encode
+from .encode import raw_encode
+
+
+def raw_decode(input_base, output_base, input_ratio, output_ratio, input_data):
+    """
+    Given an input base, an output base, input ratio, output ratio and input
+    data (as an iterable of integers), return an iterable of integers of the
+    input data decoded into the output base, using the given ratios. Interprets
+    the integer that is 1 more than the input base's max integer as a padding
+    symbol (so interpretted padding integer for decoding base64 would be 64, as
+    base64 input would be in the range 0-63).
+    """
+    # create a 'workon' copy of the input data so we don't end up changing it
+    before = list(input_data)
+    # count number of padding symbols
+    padding_length = before.count(input_base)
+    # now, replace all padding symbols with the maximmum symbol
+    '''
+    Explanation: This solution is for bases that don't match up exactly, given
+    their chosen ratios. It was inspired by the same technique that is used in
+    base85/ascii85 decoding and does not negatively impact 'perfect' aligning
+    bases such as base64.
+    '''
+    before = [(s if s != input_base else input_base - 1) for s in before]
+    # use the raw_encode function to convert the data
+    output_data = raw_encode(
+        input_base=input_base, output_base=output_base,
+        input_ratio=input_ratio, output_ratio=output_ratio, input_data=before
+    )
+    # strip off the unnecessary padding symbols if there was padding
+    [output_data.pop() for _ in range(padding_length)]
+    return output_data
 
 
 def decode(
@@ -19,29 +50,14 @@ def decode(
     Assumes standard base64-style padding using the given input padding symbol,
     but can handle unpadded input just fine.
     """
-    # create a 'workon' copy of the input data so we don't end up changing it
-    before = list(input_data)
-    # count number of padding characters
-    padding_length = before.count(input_padding)
-    # now, replace all padding characters with the maximmum symbol
-    '''
-    Explanation: This solution is for bases that don't match up exactly, given
-    their chosen ratios. It was inspired by the same technique that is used in
-    base85/ascii85 decoding and does not negatively impact 'perfect' aligning
-    bases such as base64.
-    '''
-    before = [
-        (s if s != input_padding else input_symbol_table[input_base - 1])
-        for s in before
-    ]
-    # use the encode function to convert the data
-    output_data = encode(
-        input_base=input_base, input_symbol_table=input_symbol_table,
-        output_base=output_base, output_symbol_table=output_symbol_table,
-        output_padding=None,
-        input_ratio=input_ratio, output_ratio=output_ratio,
-        input_data=before
+    # assemble input symbol table using given table + padding symbol
+    input_conversion = input_symbol_table + [input_padding]
+    # create workon copy of input data and convert symbols to raw ints
+    before = [input_conversion.index(symbol) for symbol in input_data]
+    # use raw_decode() to decode the data
+    output_data = raw_decode(
+        input_base=input_base, output_base=output_base,
+        input_ratio=input_ratio, output_ratio=output_ratio, input_data=before
     )
-    # strip off the unnecessary padding symbols if there was padding
-    [output_data.pop() for _ in range(padding_length)]
-    return output_data
+    # convert raw output data back to symbols using output symbol table
+    return [output_symbol_table[symbol] for symbol in output_data]
