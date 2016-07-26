0c20dc7d0a0d63c64e519e64528c34f1367357a8
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-07-26 23:19:40 +0100

    HOTFIX Forgot to add encoders submodule to basest __init__.py file

--- basest/__init__.py
+++ basest/__init__.py
@@ -4,7 +4,7 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-from . import core
+from . import core, encoders
 
 
-__all__ = ['core']
+__all__ = ['core', 'encoders']
