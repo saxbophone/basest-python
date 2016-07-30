5ca0726b91fa45d41b21a6d711244a16420e42bf
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-07-30 16:31:05 +0100

    Add examples of failing test cases according to criteria outlined in #19

--- tests/core/test_encode_decode_raw.py
+++ tests/core/test_encode_decode_raw.py
@@ -88,8 +88,15 @@ class TestEncodeDecodeRaw(unittest.TestCase):
         (256, 85, 4, 5, [43, 42, 41, 40, 39]),
         # Base-128 to Base-256, ratios match exactly, output base is larger
         (128, 256, 8, 7, [69, 97, 101, 100, 101, 97, 83, 97]),
+        # Same as above, but requires padding
+        (128, 256, 8, 7, [69, 97, 101, 100, 101, 97]),
         # Base-128 to Base-255, ratios are inequal, output base is larger
-        (128, 255, 9, 8, [69, 97, 101, 100, 101, 97, 83, 97, 97])
+        (128, 255, 9, 8, [69, 97, 101, 100, 101, 97, 83, 97, 97]),
+        # Same as above, but requires padding
+        (
+            128, 255, 9, 8,
+            [115, 108, 97, 114, 116, 121, 98, 97, 114, 116, 102, 97, 115, 116]
+        )
     )
     @unpack
     def test_encode_decode_raw(
