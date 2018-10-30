943c2f3074a74f5ad1f327f4b1e631ad34985190
Joshua Saxby <joshua.a.saxby@gmail.com>
2018-10-30 23:27:08 +0000

    Added MPL-2.0 license to all source code files

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016, 2018, Joshua Saxby <joshua.a.saxby@gmail.com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
 import os
 
 from pip.req import parse_requirements
