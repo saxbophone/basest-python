188a91fc658ab8edb86db3d5a23ba2eb0ae04a00
Joshua Saxby <joshua.a.saxby@gmail.com>
2018-11-02 15:08:16 +0000

    Clarify Python3 support, remove Travis-CI testing of old Py3 versions

--- setup.py
+++ setup.py
@@ -47,8 +47,6 @@ setup(
         'Intended Audience :: Telecommunications Industry',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Communications',
