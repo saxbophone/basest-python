20ed9ed8cac993ed0adfb489940cf9305b9127b6
Joshua Saxby <joshua.a.saxby@gmail.com>
2018-11-04 00:04:57 +0000

    Remove Encoder class, replace with stubbed classes from github issue

--- basest/encoders/encoder.py
+++ basest/encoders/encoder.py
@@ -6,69 +6,80 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-from __future__ import (
-    absolute_import, division, print_function, unicode_literals
-)
-
-from ..core import decode, decode_raw, encode, encode_raw
+from __future__ import absolute_import, division, print_function
 
 
-class Encoder(object):
-    # set out blank placeholders for class variables
-    input_base = None
-    output_base = None
-    input_ratio = None
-    output_ratio = None
-    input_symbol_table = None
-    output_symbol_table = None
-    padding_symbol = None
+class RawStreamingEncoder(object):
+    """
+    Base class for generator-based encoders which operate on raw ints
+    """
+    @classmethod
+    def encode(cls, data):
+        # NOTE: should be implemented in real life!
+        # this method will be a generator which implements most of the
+        # encoding work, yielding raw ints in the output base
+        raise NotImplementedError()
 
-    def encode_raw(self, input_data):
-        """
-        Encode raw data (no mapping of symbols). Use encode_raw function to
-        actually do the work.
-        """
-        return encode_raw(
-            input_base=self.input_base, output_base=self.output_base,
-            input_ratio=self.input_ratio, output_ratio=self.output_ratio,
-            input_data=input_data
-        )
+    @classmethod
+    def decode(cls, data):
+        # NOTE: should be implemented in real life!
+        # this method will be a generator which implements most of the
+        # decoding work, yielding raw ints in the input base
+        raise NotImplementedError()
 
-    def decode_raw(self, input_data):
-        """
-        Decode raw data (no mapping of symbols). Use decode_raw function to
-        actually do the work.
-        """
-        return decode_raw(
-            input_base=self.output_base, output_base=self.input_base,
-            input_ratio=self.output_ratio, output_ratio=self.input_ratio,
-            input_data=input_data
-        )
 
-    def encode(self, input_data):
-        """
-        Encode data. Use encode function to actually do the work.
-        """
-        return encode(
-            input_base=self.input_base,
-            input_symbol_table=self.input_symbol_table,
-            output_base=self.output_base,
-            output_symbol_table=self.output_symbol_table,
-            output_padding=self.padding_symbol,
-            input_ratio=self.input_ratio, output_ratio=self.output_ratio,
-            input_data=input_data
-        )
+class MappedStreamingEncoder(RawStreamingEncoder):
+    """
+    Base class for generator-based encoders which operate on mapped symbols
+    """
+    @classmethod
+    def encode(cls, data):
+        # wrap generator with another generator, one which maps the symbols
+        for symbol in super(MappedStreamingEncoder, cls).encode(data):
+            yield super(MappedStreamingEncoder, cls).map_input(symbol)
 
-    def decode(self, input_data):
-        """
-        Decode data. Use decode function to actually do the work.
-        """
-        return decode(
-            input_base=self.output_base,
-            input_symbol_table=self.output_symbol_table,
-            input_padding=self.padding_symbol,
-            output_base=self.input_base,
-            output_symbol_table=self.input_symbol_table,
-            input_ratio=self.output_ratio, output_ratio=self.input_ratio,
-            input_data=input_data
-        )
+    @classmethod
+    def decode(cls, data):
+        # wrap generator with another generator, one which maps the symbols
+        for symbol in super(MappedStreamingEncoder, cls).decode(data):
+            yield super(MappedStreamingEncoder, cls).map_output(symbol)
+
+
+class RawEncoder(RawStreamingEncoder):
+    """
+    Base class for encoders which return a list of raw ints
+    """
+    @classmethod
+    def encode(cls, data):
+        # convert generator into list
+        return list(super(Encoder, cls).encode(data))
+
+    @classmethod
+    def decode(cls, data):
+        # convert generator into list
+        return list(super(Encoder, cls).decode(data))
+
+
+class Encoder(MappedStreamingEncoder, RawEncoder):
+    """
+    Base class for encoders which return a list of symbols
+    """
+    pass
+
+
+class TypedEncoder(Encoder):
+    """
+    Base classs for encoders which return symbols coerced to a custom type
+    (for example, outputting a string rather than a list of bytes)
+    """
+    @classmethod
+    def encode(cls, data):
+        return super(TypedEncoder, cls).coerce_input(data)
+
+    @classmethod
+    def decode(cls, data):
+        return super(TypedEncoder, cls).coerce_output(data)
+
+
+class EncoderTemplate(object):
+    pass
